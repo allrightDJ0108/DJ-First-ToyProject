7531edb664e33c0ce56c5a9f0bf1de4e456e6cdb
Dajeong Choe <hers010801@gmail.com>
2023-12-22 16:06:34 +0900

    DFT-5 test : test code 추가

--- src/main/java/toy/project/boot/BootApplication.java
+++ src/main/java/toy/project/boot/BootApplication.java
@@ -2,7 +2,10 @@ package toy.project.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class BootApplication {
 
@@ -10,4 +13,8 @@ public class BootApplication {
 		SpringApplication.run(BootApplication.class, args);
 	}
 
+	@GetMapping(value = "/")
+	public String doGetMainPage(){
+		return "Hello World";
+	}
 }
